e69e5be14e9b66c97740a2b99a97cbf2aad2e155
Minjeong63 <kmj06030@naver.com>
2024-10-28 15:12:26 +0900

    Feat: 유레카 client 설정 MemberAdapter : gateway-service로 요청 보내게 지정

--- src/main/java/shop/S5G/front/adapter/MemberAdapter.java
+++ src/main/java/shop/S5G/front/adapter/MemberAdapter.java
@@ -8,7 +8,7 @@ import shop.S5G.front.dto.MemberRegistrationRequestDto;
 import shop.S5G.front.dto.MessageDto;
 
 //TODO 추후 유레카 등록 필요
-@FeignClient(value = "shop-service")
+@FeignClient(value = "gateway-service")
 public interface MemberAdapter {
 
     @PostMapping("/api/shop/member")
