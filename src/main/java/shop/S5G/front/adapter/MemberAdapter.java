7abe07021e66ef83d2f45839f11a9a076bad164a
bo <cbs1006@naver.com>
2024-10-28 13:48:38 +0900

    Feat:회원가입 구현

--- src/main/java/shop/S5G/front/adapter/MemberAdapter.java
+++ src/main/java/shop/S5G/front/adapter/MemberAdapter.java
@@ -1,8 +1,16 @@
 package shop.S5G.front.adapter;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import shop.S5G.front.dto.MemberRegistrationRequestDto;
+import shop.S5G.front.dto.MessageDto;
 
-@FeignClient(value = "shop-service")
-public class MemberAdapter {
+//TODO 추후 유레카 등록 필요
+@FeignClient(name = "shop-service", url = "http://localhost:8820")
+public interface MemberAdapter {
 
+    @PostMapping("/api/shop/member")
+    ResponseEntity<MessageDto> registerMember(@RequestBody MemberRegistrationRequestDto memberRegistrationRequestDto);
 }
