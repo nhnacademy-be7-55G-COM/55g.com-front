d56655f8e9a5a3d5bd94b024170167adbeed3f99
Minjeong63 <kmj06030@naver.com>
2024-10-29 09:32:34 +0900

    Feat: gateway, shop 연결 확인하기 위한 코드

--- src/main/java/shop/S5G/front/controller/TestController.java
+++ src/main/java/shop/S5G/front/controller/TestController.java
@@ -1,17 +1,30 @@
 package shop.S5G.front.controller;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import shop.S5G.front.adapter.TestAdapter;
 
 @Slf4j
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/test")
 public class TestController {
+    private final TestAdapter testAdapter;
+
+//    @GetMapping
+//    public String testString() {
+//        log.info("This is test log");
+//        return "This is test string";
+//    }
+
     @GetMapping
-    public String testString() {
-        log.info("This is test log");
-        return "This is test string";
+    public String test() {
+        ResponseEntity<String> response = testAdapter.test();
+        log.info(response.toString());
+        return "test success";
     }
 }
