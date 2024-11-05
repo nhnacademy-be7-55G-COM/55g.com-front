5d8308a0f859b3a7be6f64e517b9af3e8d5d0534
FlowerfulFort <skagyghk@naver.com>
2024-11-05 14:33:16 +0900

    Feat: 인증 interceptor 및 feign에 헤더 포함
    footer: 관리자용 주문 상세 업데이트 페이지

---
+++ src/main/java/shop/S5G/front/config/FeignConfig.java
@@ -0,0 +1,19 @@
+package shop.S5G.front.config;
+
+import feign.RequestInterceptor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import shop.S5G.front.utils.AuthTokenHolder;
+
+@Slf4j
+@Configuration
+public class FeignConfig {
+    @Bean
+    public RequestInterceptor globalRequestInterceptor() {
+        return requestTemplate -> {
+            log.debug("Authorization token: {}", AuthTokenHolder.getToken());
+            requestTemplate.header("Authorization", AuthTokenHolder.getToken());
+        };
+    }
+}
