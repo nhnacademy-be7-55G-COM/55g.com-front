d583a3a4f492078a6ff440ab71a3a8b48fee0cea
bo <cbs1006@naver.com>
2024-11-08 16:42:31 +0900

    Feat: 마이페이지 개인정보 변경 구현

--- src/main/java/shop/S5G/front/adapter/MemberAdapter.java
+++ src/main/java/shop/S5G/front/adapter/MemberAdapter.java
@@ -4,12 +4,14 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import shop.S5G.front.dto.jwt.TokenResponseDto;
 import shop.S5G.front.dto.member.MemberInfoResponseDto;
 import shop.S5G.front.dto.member.MemberLoginRequestDto;
 import shop.S5G.front.dto.member.MemberRegistrationRequestDto;
 import shop.S5G.front.dto.MessageDto;
+import shop.S5G.front.dto.member.MemberUpdateRequestDto;
 
 @FeignClient(name = "member", url = "${gateway.url}")
 public interface MemberAdapter {
@@ -19,4 +21,7 @@ public interface MemberAdapter {
 
     @GetMapping("/api/shop/member")
     ResponseEntity<MemberInfoResponseDto> getMemberInfo();
+
+    @PutMapping("/api/shop/member")
+    ResponseEntity<MessageDto> updateMember(@RequestBody MemberUpdateRequestDto memberUpdateRequestDto);
 }
--- src/main/java/shop/S5G/front/service/member/MemberService.java
+++ src/main/java/shop/S5G/front/service/member/MemberService.java
@@ -1,12 +1,12 @@
 package shop.S5G.front.service.member;
 
-import jakarta.servlet.http.HttpServletResponse;
 import shop.S5G.front.dto.member.MemberInfoResponseDto;
-import shop.S5G.front.dto.member.MemberLoginRequestDto;
 import shop.S5G.front.dto.member.MemberRegistrationRequestDto;
 import shop.S5G.front.dto.MessageDto;
+import shop.S5G.front.dto.member.MemberUpdateRequestDto;
 
 public interface MemberService {
     MessageDto registerMember(MemberRegistrationRequestDto memberRegistrationRequestDto);
     MemberInfoResponseDto getMemberInfo();
+    MessageDto updateMember(MemberUpdateRequestDto updateMemberRequestDto);
 }
