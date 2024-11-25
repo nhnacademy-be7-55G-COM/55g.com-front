1da861880722f502ef2c69b62d38161edc44a433
bo <cbs1006@naver.com>
2024-11-25 15:37:39 +0900

    Refactor: 마이 페이지 분리

--- src/main/java/shop/s5g/front/controller/mypage/MyPageController.java
+++ src/main/java/shop/s5g/front/controller/mypage/MyPageController.java
@@ -41,46 +41,56 @@ public class MyPageController {
     public String registerAddress(@Valid @ModelAttribute AddressRequestDto addressRequestDto,
         BindingResult bindingResult, Model model) {
         if (bindingResult.hasErrors()) {
-            return "redirect:/mypage#deliveryAddress";
+            return "redirect:/mypage/address";
         }
         addressService.addAddress(addressRequestDto);
-        return "redirect:/mypage#deliveryAddress";
+        return "redirect:/mypage/address";
     }
 
     @PostMapping("/mypage/deleteAddress")
     public String deleteAddress(@RequestParam Long addressId) {
         addressService.deleteAddress(addressId);
-        return "redirect:/mypage#deliveryAddress";
+        return "redirect:/mypage/address";
     }
 
     @PostMapping("/mypage/updateAddress")
     public String updateAddress(@RequestParam Long addressId,
         @ModelAttribute AddressUpdateRequestDto requestDto, Model model) {
         addressService.updateAddress(addressId, requestDto);
-        return "redirect:/mypage#deliveryAddress";
+        return "redirect:/mypage/address";
     }
 
     @GetMapping("/mypage")
     public String myPage(Model model) {
-        MemberInfoResponseDto responseDto = memberService.getMemberInfo();
-        List<AddressResponseDto> addresses = addressService.getAddresses();
-        model.addAttribute("member", responseDto);
-        model.addAttribute("addresses", addresses);
-
 //        return "mypage";
         return "layout/mypage";
     }
 
+    @GetMapping("/mypage/personal-info")
+    public String personalInfo(Model model) {
+        MemberInfoResponseDto responseDto = memberService.getMemberInfo();
+        model.addAttribute("member", responseDto);
+        return "mypage/personal-info";
+    }
+
+    @GetMapping("/mypage/address")
+    public String deliveryAddress(Model model) {
+        List<AddressResponseDto> addresses = addressService.getAddresses();
+        model.addAttribute("addresses", addresses);
+        return "mypage/address";
+    }
+
     @PostMapping("/mypage/changeInfo")
     public String changeMemberInfo(@Valid @ModelAttribute MemberUpdateRequestDto requestDto,
-        BindingResult bindingResult, Model model) {
+        BindingResult bindingResult, RedirectAttributes redirectAttributes) {
 
         if (bindingResult.hasErrors()) {
-            return "redirect:/mypage#personalInfo";
+            redirectAttributes.addFlashAttribute("error", "양식을 확인해주세요.");
+            return "redirect:/mypage/personal-info";
         }
         memberService.updateMember(requestDto);
-
-        return "redirect:/mypage#personalInfo";
+        redirectAttributes.addFlashAttribute("success","변경에 성공했습니다.");
+        return "redirect:/mypage/personal-info";
     }
 
     @PostMapping("/mypage/changePassword")
@@ -90,20 +100,30 @@ public class MyPageController {
 
         if (bindingResult.hasErrors()) {
             redirectAttributes.addFlashAttribute("error", "비밀번호 형식을 확인해주세요");
-            return "redirect:/mypage#personalInfo";
+            return "redirect:/mypage/personal-info";
         }
         try{
             MessageDto messageDto = memberService.changePassword(requestDto);
             redirectAttributes.addFlashAttribute("success", messageDto.message());
-
         }
         catch (Exception e){
             redirectAttributes.addFlashAttribute("error", "비밀번호 변경을 실패했습니다.");
-            return "redirect:/mypage#personalInfo";
+            return "redirect:/mypage/personal-info";
         }
 
+        return "redirect:/mypage/personal-info";
+    }
 
-        return "redirect:/mypage#personalInfo";
+    @GetMapping("/mypage/grade")
+    public String grade(Model model) {
+        MemberInfoResponseDto responseDto = memberService.getMemberInfo();
+        model.addAttribute("member", responseDto);
+        return "mypage/grade";
+    }
+
+    @GetMapping("/mypage/deleteAccount")
+    public String delete(){
+        return "mypage/delete-account";
     }
 
     @PostMapping("/mypage/deleteAccount")
@@ -111,6 +131,7 @@ public class MyPageController {
         cartService.removeAccount();
         memberService.deleteMember();
         authService.logoutMember(request, response);
+
         return "redirect:/";
     }
 
