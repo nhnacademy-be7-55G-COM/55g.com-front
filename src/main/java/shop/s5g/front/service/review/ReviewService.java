bbced9f0fb4edfc6438266ea3c3c50704654e814
Minjeong63 <kmj06030@naver.com>
2024-11-25 11:24:49 +0900

    Feat: 사용자별 리뷰 내역 조회

--- src/main/java/shop/s5g/front/service/review/ReviewService.java
+++ src/main/java/shop/s5g/front/service/review/ReviewService.java
@@ -1,8 +1,14 @@
 package shop.s5g.front.service.review;
 
+import org.springframework.data.domain.Pageable;
 import shop.s5g.front.dto.MessageDto;
+import shop.s5g.front.dto.PageResponseDto;
 import shop.s5g.front.dto.review.CreateReviewRequestDto;
+import shop.s5g.front.dto.review.ReviewResponseDto;
 
 public interface ReviewService {
+
     MessageDto addReview(CreateReviewRequestDto createReviewRequestDto);
+
+    PageResponseDto<ReviewResponseDto> getReviewList(Pageable pageable);
 }
